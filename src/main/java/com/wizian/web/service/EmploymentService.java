5b9834042c753b4103026aff579cdc236cb4b49a
loas <shinjin1023@naver.com>
2024-05-09 12:41:56 +0900

    좌측nav추가, 상담 신청, 상담사소개 html 추가

---
+++ src/main/java/com/wizian/web/service/EmploymentService.java
@@ -0,0 +1,15 @@
+package com.wizian.web.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmploymentService {
+
+	public List<Map<String, Object>> boardList() {
+		return null;
+	}
+
+}
