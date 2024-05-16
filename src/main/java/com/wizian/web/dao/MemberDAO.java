b26c150b93aad91ddd01e15e47978a216855e786
Jinsu Jung <jjs17340@gmail.com>
2024-05-16 10:03:28 +0900

    로그인 구현 중

---
+++ src/main/java/com/wizian/web/dao/MemberDAO.java
@@ -0,0 +1,12 @@
+package com.wizian.web.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Mapper
+public interface MemberDAO {
+
+}
