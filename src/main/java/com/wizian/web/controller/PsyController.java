4e3d40e2aefb460e51dbeb932530eb681d0c22df
ChaeYS <dudtjs831@gmail.com>
2024-05-20 18:01:03 +0900

    일정 추가 모달창에서 시간 선택하기 추가

--- src/main/java/com/wizian/web/controller/PsyController.java
+++ src/main/java/com/wizian/web/controller/PsyController.java
@@ -1,10 +1,20 @@
 package com.wizian.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.wizian.web.dto.UserDTO;
+import com.wizian.web.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PsyController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/psycoun_proc")
 	public String index() {
@@ -19,7 +29,12 @@ public class PsyController {
 	}
 	
 	@GetMapping("/psycoun_app")
-	public String showPsycounAppPage() {
+	public String showPsycounAppPage(HttpSession session, Model model) {
+		
+		String userId = (String) session.getAttribute("userId");
+		System.out.println(userId);
+		UserDTO userInfo = userService.userInfo(userId);
+		model.addAttribute("userInfo", userInfo); // 사용자 정보
 		return "psycoun_app";
 	}
 	
