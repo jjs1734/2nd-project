2c50ad7a4b955222e7c16a17b38c9e275c0dbf59
loas <shinjin1023@naver.com>
2024-05-25 17:35:46 +0900

    younsun db insert clear

--- src/main/java/com/wizian/web/dto/PsycounDTO.java
+++ src/main/java/com/wizian/web/dto/PsycounDTO.java
@@ -1,4 +1,22 @@
 package com.wizian.web.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class PsycounDTO {
+	
+	private String name;
+	private String na;
+	private String sc;
+    private String phone;
+    private String email;
+    private boolean agree;
+    private int time;
+    private String date;
+    private String userId;
+    private String date1;
+    
 }
