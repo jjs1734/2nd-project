bd40242fc9e83a79ac1275cba41b0a882457350b
loas <shinjin1023@naver.com>
2024-05-27 18:57:51 +0900

    younsun db insert

--- src/main/java/com/wizian/web/dao/PsycounDAO.java
+++ src/main/java/com/wizian/web/dao/PsycounDAO.java
@@ -1,40 +1,14 @@
 package com.wizian.web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.wizian.web.dto.PsycounDTO;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
+@Mapper
 @Repository
-public class PsycounDAO {
-	
-	@Autowired
-    private JdbcTemplate jdbcTemplate;
+public interface PsycounDAO {
 
-    public List<PsycounDTO> findAll() {
-        String sql = "SELECT * FROM PSY_EXAM_VISIT_REQUEST";
-        return jdbcTemplate.query(sql, new PsyExamVisitRequestRowMapper());
-    }
+	public int submitAppointment(PsycounDTO psycounDTO);
 
-    private static final class PsyExamVisitRequestRowMapper implements RowMapper<PsycounDTO> {
-        @Override
-        public PsycounDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-            PsycounDTO request = new PsycounDTO();
-            request.setKey(rs.getString("key"));
-            request.setStudNo(rs.getString("stud_no"));
-            request.setVisitResYmd(rs.getString("visit_res_ymd"));
-            request.setVisitResCd(rs.getString("visit_res_cd"));
-            request.setConVisitYmd(rs.getString("con_visit_ymd"));
-            request.setConVisitCd(rs.getString("con_visit_cd"));
-            request.setCounProCd(rs.getString("coun_pro_cd"));
-            request.setPsyExam(rs.getString("psy_exam"));
-            return request;
-        }
-    }
 }
--- src/main/java/com/wizian/web/service/PsycounService.java
+++ src/main/java/com/wizian/web/service/PsycounService.java
@@ -1,21 +1,10 @@
 package com.wizian.web.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.wizian.web.dao.PsycounDAO;
-import com.wizian.web.dto.PsycounDTO;
 
 @Service
 public class PsycounService {
 
-	@Autowired
-    private PsycounDAO dao;
 
-    public List<PsycounDTO> getAllVisitRequests() {
-        return dao.findAll();
-    }
     
 }
