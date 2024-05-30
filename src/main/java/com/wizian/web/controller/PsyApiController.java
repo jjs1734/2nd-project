23574dd928e34c0e13e34277b22cfaecb6deecd2
loas <shinjin1023@naver.com>
2024-05-30 09:21:11 +0900

    1

--- src/main/java/com/wizian/web/controller/PsyApiController.java
+++ src/main/java/com/wizian/web/controller/PsyApiController.java
@@ -1,13 +1,9 @@
 package com.wizian.web.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wizian.web.dto.PsycounDTO;
 import com.wizian.web.service.PsycounService;
 
 @RestController
